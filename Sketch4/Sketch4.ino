093a78adc1fad07ec5e28cfcbdd62c443f1a8aa4
duna <sas@maa.ee>
2018-04-14 22:09:11 +0400

    Add project files.

---
+++ Sketch4/Sketch4.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Sketch4.ino
+ Created:	4/14/2018 10:09:01 PM
+ Author:	Администратор
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
